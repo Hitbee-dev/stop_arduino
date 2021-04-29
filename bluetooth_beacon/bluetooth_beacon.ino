5989817cc4d6d084da0dd0d13ba8b239b6a9b03b
K9714 <kskim.hci@gmail.com>
2021-04-29 18:42:24 +0900

    210429A

--- bluetooth_beacon/bluetooth_beacon.ino
+++ bluetooth_beacon/bluetooth_beacon.ino
@@ -1,75 +1,86 @@
-#include <SoftwareSerial.h> 
-#include <string.h>
-#define TX 14
-#define RX 15
+/*  
+ *   Title. HNU Capstone :: STOP - Bluetooth Beacon
+ *   Autor. K9714
+ *   Date.  21. 04. 28
+ */ 
 
-//UUID 2E234454CF6D4A0FADF2F4911BA9FFA6
-SoftwareSerial BTSerial(TX, RX);
-boolean checker = false;
-unsigned long time=0,pre_time=0,buf=0;
-byte bt_in;
-char bf;
-int T_count=0;
-byte data;
-char buff[200];
-unsigned char buff_idx = 0;
-unsigned char isParse = 0;
-char *uuid= "2E234454CF6D4A0FADF2F4911BA9FFA6";
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial HM10(2,3); // RX, TX
+
+// Check Sec
+#define SLEEP_TIME 15 // (5, 10, 15) ms
+#define MAX_COUNT 20
+
+// Check MAC
+String CHECK_UUID = "2E234454CF6D4A0FADF2F4911BA9FFA6";
+
+// Parse Buffer
+char buff[255] = {0};
+char buff_idx = 0;
+
+// counting
+int success = 0, fail = 0;
+int count = MAX_COUNT;
+bool check = false;
+bool start = false;
+
 
 void setup() {
-   Serial.begin(9600);
-   pinMode(RX, INPUT);    //UART pin for Bluetooth
-   pinMode(TX, OUTPUT);
-   BTSerial.begin(9600);
-   BTSerial.println("AT+ROLE1");
-   BTSerial.println("AT+IMME1");//IMME[] 모듈 동작 타입 설정
+  //기본 통신속도는 9600입니다.
+  Serial.begin(9600);
+  HM10.begin(9600);
+  Serial.println("Arduino UNO Start!");
 }
 
-void loop(){
-  if (BTSerial.available()) {
-    Serial.write(BTSerial.read());
-  }
-  if (Serial.available()) {
-    BTSerial.write(Serial.read());
-  }
- /*
-  if (BTSerial.available()) {
-    char c = BTSerial.read();
-    Serial.print(c);
-    /*buff[buff_idx++] = c;
-    
-    if (c == '\n') {
-      buff[buff_idx++] = '\0';
-      buffParse();
-    }*/
-  
-}
-
-
-void buffParse() {
-  char *ptr;
-  unsigned char idx;
-  ptr = strtok(buff, "+");
-  ptr += strlen(ptr) + 1;
-  if (!strncmp(ptr, "DISCE", 5)) {
-  }
-  else if (!strncmp(ptr, "DISC", 4)) {
-    ptr = strtok(ptr, ":");
-    while (ptr != NULL) {
-      if (strlen(ptr) == 32) {
-      // Serial.println(ptr);
-       int com = strcmp(ptr,uuid);
-       if(com == 0){ 
-          checker = true;
-          }
-        else{
-        
-          checker = false;
-           }
-      }
-      ptr = strtok(NULL, ":");
-      
+void loop() {
+  // Buffer check
+  while (HM10.available()) {
+    char data = HM10.read();
+    buff[buff_idx++] = data;
+    if (data == '\n') {
+      buff[buff_idx] = '\0';
+      parse_buffer();
     }
+  }
+  if (count > 0 && !start) {
+    delay(SLEEP_TIME);
+    count--;
+    Serial.print("MSG COUNT : ");
+    Serial.println(MAX_COUNT - count);
+    HM10.println("AT+DISI?");
+    start = true;
+  }
+  else if (count == 0 && !start) {
+    count--;
+    Serial.print("SUCCESS : ");
+    Serial.println(success);
+    Serial.print("Fail : ");
+    Serial.println(fail);
+  }
+  
+  while (Serial.available()) {
+    byte data = Serial.read();
+    HM10.write(data);
+  }
+}
+
+
+void parse_buffer() {
+  String line = buff;
+  //Serial.println();
+  if (line.substring(0, 8).equals("OK+DISC:")) {
+    String mac = line.substring(17, 49);
+    //Serial.println(mac);
+    if (mac.equals(CHECK_UUID)) {
+      check = true;
+    }
+  }
+  if (line.substring(0, 8).equals("OK+DISCE")) {
+    check ? success++ : fail++;
+    check = false;
+    start = false;
   }
   buff_idx = 0;
 }
